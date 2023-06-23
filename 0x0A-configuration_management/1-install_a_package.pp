fa64136f93b85358c0c6baafb774777332df72c1
NebihaDilsebo <shegolenur@hotmail.com>
2023-06-23 10:26:02 -0700

    0x0A-configuration_management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,19 +1,5 @@
-package { 'Flask':
+# installs the package puppet-lint
+package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
-}
-
-exec { 'export_flask_version':
-  command     => '/usr/bin/flask --version',
-  path        => '/usr/local/bin:/usr/bin:/bin',
-  refreshonly => true,
-  subscribe   => Package['Flask'],
-  notify      => Exec['display_flask_version'],
-}
-
-exec { 'display_flask_version':
-  command     => '/usr/bin/flask --version',
-  path        => '/usr/local/bin:/usr/bin:/bin',
-  logoutput   => true,
-  refreshonly => true,
 }
